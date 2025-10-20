c9943d5fbb2612022740b3d9613be30315977dbe
kaua santos <kaua7santos7oliveiraa@gmail.com>
2025-10-20 16:34:43 -0300

    função deletar no streamlit

--- front-end/app.py
+++ front-end/app.py
@@ -26,7 +26,7 @@ elif menu == "Adicionar produto":
     s.subheader('➕ Adicionar produto')
     nome = s.text_input("Nome do produto")
     categoria = s.text_input("Categoria")
-    preco = s.number_input("Preço", min_value=0.0, step=0.01)
+    preco = s.number_input("Preço", min_value=0.0, step=0.01, format="%.2f")
     quantidade = s.number_input("Quantidade", min_value=0, step=1)
 
     if s.button("Salvar produto"):
@@ -41,3 +41,24 @@ elif menu == "Adicionar produto":
             s.success("Produto adicionado com sucesso")
         else:
             s.error("Erro ao adicionar o produto")
+
+elif menu == "Atualizar produto":
+    s.subheader("Atualizar produto")
+    id_produto = s.number_input("ID do produto a atualizar", min_value=1, step=1)
+    novo_preco = s.number_input("Novo preço", min_value=0.0, step=0.01)
+    nova_quantidade = s.number_input("Nova quantidade", min_value=0, step=1)
+
+    if s.button("Atualizar"):
+        params = {
+            "novo_preco": novo_preco,
+            "nova_quantidade": nova_quantidade
+        }
+        response = r.put(f"{API_URL}/loja/{id_produto}", params=params)
+        if response.status_code == 200:
+            data = response.json()
+            if "erro" in data:
+                s.warning(data["erro"])
+            else:
+                s.success("Produto atualizado com sucesso 😁👍")
+        else:
+            s.error("Erro ao atualizar produto ❌")
